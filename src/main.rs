10500eadf2967037863f7b2d498b7e1f0903dc02
AdamOlsson <olsson.95_@hotmail.com>
2024-08-23 08:07:30 +0200

    moving pipes

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,79 @@
-use game_engine::engine::{physics_engine::{collision::CollisionBody, integrator::verlet::VerletIntegrator}, renderer_engine::{shapes::circle::Circle, vertex::Vertex}, Simulation};
-use cgmath::{Vector3, Zero};
+use cgmath::{Vector2, Vector3, Zero};
 use winit::dpi::PhysicalSize;
-use game_engine::engine::run::run;
+use game_engine::engine::physics_engine::collision::collision_body::CollisionBody;
+use game_engine::engine::{physics_engine::integrator::verlet::VerletIntegrator, run::run, Simulation};
 
 struct FlappyBird {
-    player: CollisionBody,
     integrator: VerletIntegrator,
+    dt: f32,
+    window_size: Vector2<f32>, 
+}
 
-    // Render data
-    colors: Vec<Vector3<f32>>,
-    indices: Vec<u16>,
-    vertices: Vec<Vertex>,
-    num_indices: u32,
+const PIPE_WIDTH: f32 = 250.0;
+const PIPE_PAIR_DISTANCE_Y: f32 = 150.0;
+
+impl FlappyBird {
+    fn new(window_size: &PhysicalSize<u32>) -> Self {
+        let dt = 0.001;
+        let gravity: f32 = 1000000.0;
+        let accelleration = Vector3::new(0.0, -gravity, 0.0);
+
+        let player = CollisionBody::circle(0, Vector3::zero(), accelleration, 
+            Vector3::zero(), Vector3::zero(), 50.0, Vector3::new(255.0,0.0,0.0)); 
+
+        // TODO: Create ID incrementer
+        let (top_pipe, bot_pipe) = Self::create_pipe_pair(100.0, PIPE_WIDTH,
+            PIPE_PAIR_DISTANCE_Y, 0.25, window_size.height as f32, -2.0);
+
+        let integrator = VerletIntegrator::new(f32::MAX, vec![player, top_pipe, bot_pipe]);
+
+        let window_size = Vector2::new(window_size.width as f32, window_size.height as f32);
+
+        Self {
+            integrator, dt, window_size,
+        }
+    }
+
+    fn create_pipe_pair(
+        x: f32, pipe_width:f32, hole_size:f32, 
+        hole_weight:f32, window_height:f32, velocity_x: f32
+    ) -> (CollisionBody, CollisionBody) {
+        
+        let top_weight = 1.0 - hole_weight;
+        let bot_weight = 1.0 + hole_weight;
+
+        let velocity = Vector3::new(velocity_x,0.0,0.0);
+        let acceleration = Vector3::zero();
+        let color = Vector3::new(0.0,255.0,0.0);
+
+        let top_position = Vector3::new(x, 
+            window_height - (window_height-hole_size/2.0)*top_weight, 0.0);
+        let top_prev_position = top_position - velocity;
+        let top = CollisionBody::rectangle(1, velocity, acceleration, top_prev_position,
+            top_position, pipe_width, window_height*2.0, color);
+
+        let bot_position = Vector3::new(x, -window_height, 0.0);
+        let bot_prev_position = bot_position - velocity;
+        let bot = CollisionBody::rectangle(2, velocity, acceleration, bot_prev_position,
+            bot_position, pipe_width,  (window_height - hole_size/2.0)*bot_weight, color) ;
+
+        (top, bot)
+    }
 }
 
 impl Simulation for FlappyBird {
 
-    fn new(_window_size: &PhysicalSize<u32>) -> Self {
-        let player = CollisionBody::new(0, Vector3::zero(), Vector3::zero(), Vector3::zero(), 50.0); 
-  
-        let integrator = VerletIntegrator::new(f32::MAX, vec![Vector3::zero()], vec![player]);
-        let colors = vec![Vector3::new(255.0,0.0,0.0)];
-
-        let vertices = Circle::compute_vertices([0.0,0.0,0.0], 1.0);
-        let indices = Circle::compute_indices();
-        let num_indices = Circle::get_num_indices();
-
-        Self {
-            player, integrator,
-            colors, indices, vertices, num_indices
-        }
-    }
-
     fn update(&mut self) {
+        self.integrator.update(self.dt);
+        let bodies = self.integrator.get_bodies_mut();
+        let player = &bodies[0];
+        if player.position.y < -self.window_size.y {
+            panic!("Game Over!");
+        } 
     }
 
-
-    fn get_bodies(&self) -> &Vec<game_engine::engine::physics_engine::collision::CollisionBody> {
+    fn get_bodies(&self) -> &Vec<CollisionBody> {
         &self.integrator.get_bodies()
-    }
-
-    fn get_vertices(&self) -> &Vec<game_engine::engine::renderer_engine::vertex::Vertex> {
-        &self.vertices
-    }
-
-    fn get_indices(&self) -> &Vec<u16> {
-        &self.indices
-    }
-    
-    fn get_colors(&self) -> &Vec<Vector3<f32>> {
-        &self.colors
     }
 
     fn get_num_active_instances(&self) -> u32 {
@@ -60,17 +84,13 @@ impl Simulation for FlappyBird {
         1
     }
 
-    fn get_num_indices(&self) -> u32 {
-        self.num_indices
-    }
-
-    fn log_performance(&mut self) {
-        todo!();
+    fn jump(&mut self) {
+        self.integrator.set_velocity_y(0, 20.0);
     }
 }
 
 fn main() {
     let window_size = PhysicalSize::new(600,600);
-    let mut flappy_bird = FlappyBird::new(&window_size);
-    pollster::block_on(run(&mut flappy_bird, window_size));
+    let flappy_bird = FlappyBird::new(&window_size);
+    pollster::block_on(run(flappy_bird, window_size, 20));
 }
